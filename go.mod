1b481abdd54eaac9f66a56a20a9a29c39a9976f4
aburizalpurnama <purnamar9@gmail.com>
2022-02-19 18:18:53 +0700

    Do some major upgrade on module

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/aburizalpurnama/go-say-hello
+module github.com/aburizalpurnama/go-say-hello/v2
 
 go 1.16
--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Hai, my name is " + name
+func SayHello() string {
+	return "Hello"
 }
